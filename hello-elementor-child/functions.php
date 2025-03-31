f70683e6bf05b7bd5aaa9fed4b9822017f5c7859
Pranshu Singh <115446879+PranshuSingh19@users.noreply.github.com>
2025-03-31 10:20:51 +0530

    Update functions.php

--- hello-elementor-child/functions.php
+++ hello-elementor-child/functions.php
@@ -2,7 +2,8 @@
 <?php
 function enqueue_style(){
 
-    wp_enqueue_style('parent-theme', get_template_directory_uri() . '/style.css/');
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css/');
+    wp_enqueue_style('clild-style', get_template_directory_uri() . '/style.css/');
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_style');
 
